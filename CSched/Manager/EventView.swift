20249b5378fdc6b97dd66026e3c4d82e6ec4441b
Isaac Raval <23413465+ByteSizeCode@users.noreply.github.com>
2020-04-23 00:07:39 -0700

    Update EventView.swift

--- CSched/Manager/EventView.swift
+++ CSched/Manager/EventView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 import Foundation
 
 struct ModalView: View {
-    let ENABLE_SAVE_EVENT_DATE = true
+    let ENABLE_SAVE_EVENT_DATE = false
     let ENABLE_CLEAR_BUTTON = true
     @State var showingAlert = false
     @Binding var presentedAsModal: Bool
